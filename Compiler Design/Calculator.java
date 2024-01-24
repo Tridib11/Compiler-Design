992a8dc601340961d893e8346b9d917c52868c8f
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2024-01-24 05:12:24 +0000

    files modifeid

--- Compiler Design/Calculator.java
+++ Compiler Design/Calculator.java
@@ -1,34 +1,3 @@
-// import java.util.*;
-// public class Calculator{
-//     public static void main(String args[]){
-//         Scanner sc=new Scanner(System.in);
-//         System.out.println("Enter the first number ");
-//         int num1=sc.nextInt();
-//         System.out.println("Enter the Second number ");
-//         int num2=sc.nextInt();
-//         char ch = sc.nextLine().trim().charAt(0);
-//         int ans=0;
-        
-//             switch(ch){
-//                 case '+':
-//                 ans = num1+num2;
-                
-//                 break;
-//                 case '-':
-//                 ans=num1-num2;
-          
-//                 break;
-
-//                 default:
-//                 System.out.println("sorry wrong operator");
-//                 break;
-//             }
-        
-        
-//     }
-// }
-
-
 import java.util.*;
 
 public class Calculator {
@@ -44,12 +13,16 @@ public class Calculator {
         switch (ch) {
             case '+':
                 ans = num1 + num2;
-                System.out.println(ans);
+                System.out.println("ANS "+ans);
                 break;
             case '-':
                 ans = num1 - num2;
-                System.out.println(ans);
+                System.out.println("ANS "+ans);
                 break;
+            case '*':
+                ans=num1*num2;
+                System.out.println("ANS "+ans);
+                break;                
             default:
                 System.out.println("sorry wrong operator");
                 break;
